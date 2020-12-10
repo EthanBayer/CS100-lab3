cb96a9b37ef0c7af3a036a2a3d6959926889d444
EthanBayer <bayeres1871@gmail.com>
2020-12-10 15:44:20 -0800

    Added all files from lab3.

---
+++ build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Ethan Bayer/CS100-Labs/lab-03---github-flow-bguev006-ebaye01/googletest
+# Build directory: C:/Users/Ethan Bayer/CS100-Labs/lab-03---github-flow-bguev006-ebaye01/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Ethan Bayer/CS100-Labs/lab-03---github-flow-bguev006-ebaye01/googletest/googletest
+# Build directory: C:/Users/Ethan Bayer/CS100-Labs/lab-03---github-flow-bguev006-ebaye01/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
